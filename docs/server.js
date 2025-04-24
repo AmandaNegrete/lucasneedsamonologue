d61655592c23fe3475806060714e21bec3a97e18
Amanda! :) <anegret1@umbc.edu>
2025-04-24 17:33:21 -0400

    please work

--- docs/server.js
+++ docs/server.js
@@ -11,7 +11,7 @@ app.use(express.static('docs'));
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'docs', 'index.html'));  // Ensure this path matches where your index.html is
+    res.sendFile(path.join(__dirname, 'docs', 'index.html'));  // Corrected path
 });
 
 const db = new sqlite3.Database('./monologues.db');
