43b8681e6656d6dbb30ea2c967b2dda33d94cc66
Amanda! :) <anegret1@umbc.edu>
2025-04-24 18:15:02 -0400

    pushing

--- docs/server.js
+++ docs/server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 // Serve static files from the 'docs' folder (where your HTML, CSS, JS files are)
-app.use(express.static('docs')); 
+app.use(express.static(__dirname)); 
 
 // Serve index.html for the root route
 app.get('/', (req, res) => {
